f885484e51d493d64549274c31090264b7267bb9
= <jsaad41@gmail.com>
2022-06-11 17:33:15 +1000

    enemy be there, find neighbours

---
+++ Assets/Scripts/Objects/Character.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Character
+{
+    public int maxEnergy;
+    public int currentEnergy;
+    public float playerSpeed;
+    public int range;
+}
