bd4d569696939871029b11c6ce683538041d4c5a
= <jsaad41@gmail.com>
2022-07-01 21:04:38 +1000

    costings and start of game flow

---
+++ Assets/Scripts/Objects/ScriptableObjects/CoinStoreData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinStoreData : ScriptableObject
+{
+    public int capacity = 200;
+}
---
+++ Assets/Scripts/Objects/ScriptableObjects/GameData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/GameDataScriptableObject", order = 1)]
+public class GameData : ScriptableObject
+{
+    public int[] defenceGoldIncrease;
+    public int[] attackGoldIncrease;
+}
