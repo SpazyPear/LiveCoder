3813be46e73ce15a6468e8160a0627a6cb5c95cd
= <jsaad41@gmail.com>
2022-07-13 20:30:41 +1000

    wall better

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -20,9 +20,24 @@ public class Wall : Entity
 {
     async void Start()
     {
-        await Task.Delay(200);
+        await Task.Delay(100);
 
-            if (State.GridContents[gridPos.x, gridPos.y + 1].Entity && State.GridContents[gridPos.x, gridPos.y + 1].Entity.GetComponentInChildren<Wall>() || State.GridContents[gridPos.x, gridPos.y - 1].Entity && State.GridContents[gridPos.x, gridPos.y - 1].Entity.GetComponentInChildren<Wall>())
-                transform.parent.eulerAngles = new Vector3(0, 90, 0);
+        CheckWallRotation();
+            
     }
+    
+    public void CheckWallRotation()
+    {
+        if (State.GridContents[gridPos.x, gridPos.y + 1].Entity && State.GridContents[gridPos.x, gridPos.y + 1].Entity.GetComponentInChildren<Wall>() || State.GridContents[gridPos.x, gridPos.y - 1].Entity && State.GridContents[gridPos.x, gridPos.y - 1].Entity.GetComponentInChildren<Wall>())
+        {
+            transform.parent.eulerAngles = new Vector3(0, 90, 0);
+            if (State.GridContents[gridPos.x, gridPos.y + 1].Entity)
+                State.GridContents[gridPos.x, gridPos.y + 1].Entity.SendMessage("CheckWallRotation");
+            if (State.GridContents[gridPos.x, gridPos.y - 1].Entity)
+                State.GridContents[gridPos.x, gridPos.y - 1].Entity.SendMessage("CheckWallRotation");
+
+        }
+    }
+
+    
 }
